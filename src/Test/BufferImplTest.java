5099be6ac69483e7e0b6845762878f3f4342b4f4
yletaillanter <y.letaillanter@gmail.com>
2014-11-09 17:28:38 +0100

    Test

--- src/Test/BufferImplTest.java
+++ src/Test/BufferImplTest.java
@@ -1,9 +1,13 @@
 package Test;
+import Moteur.Buffer;
+import Moteur.BufferImpl;
+
+import java.util.List;
+
 import static org.mockito.Mockito.*;
 
 /**
  * Created by Yoann on 09/11/2014.
  */
 public class BufferImplTest {
-
 }
---
+++ src/Test/MoteurImplTest.java
@@ -0,0 +1,16 @@
+package Test;
+
+import Moteur.*;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by Yoann on 09/11/2014.
+ */
+public class MoteurImplTest {
+
+    Buffer mockBuffer = mock(BufferImpl.class);
+    Selection mockedSelection = mock(SelectionImpl.class);
+    PressePapier mockedPressePapier = mock(PressePapierImpl.class);
+
+}
