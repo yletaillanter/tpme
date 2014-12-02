ab61f88c05695c54dd3ff3d41523aa157cad9ffb
yletaillanter <y.letaillanter@gmail.com>
2014-12-02 17:51:15 +0100

    CommandeTest OK.

--- src/Test/CommandeTest.java
+++ src/Test/CommandeTest.java
@@ -7,8 +7,10 @@ import Memento.Enregistreur;
 import Memento.EnregistreurImpl;
 import Moteur.MoteurEdition;
 import Moteur.MoteurEditionImpl;
-import commandes.Commande;
-import commandes.CommandeInsererTexte;
+import UndoRedo.UndoRedoManager;
+import UndoRedo.UndoRedoManagerImpl;
+import commandes.*;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -20,18 +22,99 @@ public class CommandeTest {
     IHMImpl mockitoIHM = Mockito.mock(IHMImpl.class);
     MoteurEdition mockitoMoteur = Mockito.mock(MoteurEditionImpl.class);
     Enregistreur mockitoEnregistreur = Mockito.mock(EnregistreurImpl.class);
-    Commande commandeInserer = new CommandeInsererTexte(mockitoMoteur,mockitoIHM,mockitoEnregistreur);
+    UndoRedoManager mockitoManager = Mockito.mock(UndoRedoManagerImpl.class);
+    Commande commandeInserer, commandeCopier, commandeColler, commandeCouper, commandeSelectionner, commandeSupDroite,
+            commandeSupGauche, commandeStart, commandeStop, commandePlay, commandeUndo, commandeRedo;
+
+    @Before
+    public void setUp(){
+        commandeInserer = new CommandeInsererTexte(mockitoMoteur,mockitoIHM,mockitoEnregistreur);
+        commandeCopier = new CommandeCopierTexte(mockitoMoteur,mockitoEnregistreur);
+        commandeColler = new CommandeCollerTexte(mockitoMoteur, mockitoEnregistreur);
+        commandeCouper =  new CommandeCouperTexte(mockitoMoteur, mockitoEnregistreur);
+        commandeSelectionner =  new CommandeSelectionnerTexte(mockitoMoteur, mockitoIHM, mockitoEnregistreur);
+        commandeSupDroite  = new CommandeSupprimerTexteDroite(mockitoMoteur, mockitoEnregistreur);
+        commandeSupGauche = new CommandeSupprimerTexteGauche(mockitoMoteur, mockitoEnregistreur);
+        commandeStart = new CommandeStart(mockitoEnregistreur);
+        commandeStop = new CommandeStop(mockitoEnregistreur);
+        commandePlay = new CommandePlay(mockitoEnregistreur);
+        commandeUndo = new CommandeUndo(mockitoManager);
+        commandeRedo = new CommandeRedo(mockitoManager);
+    }
 
     @Test
     public void testInserer(){
-        // Ajout de la commande
-        mockitoIHM.addCommand(commandeInserer, "Insert");
-        // verif de l'ajout de la commande
-        verify(mockitoIHM).addCommand(commandeInserer, "Insert");
-
         //Appel de la commande
         commandeInserer.execute();
-        verify(mockitoMoteur).inserer(null,false);
+        verify(mockitoMoteur).inserer(null, false);
+    }
+
+    @Test
+    public void testCopier(){
+        commandeCopier.execute();
+        verify(mockitoMoteur).copier();
+    }
+
+    @Test
+    public void testColler(){
+        commandeColler.execute();
+        verify(mockitoMoteur).coller();
+    }
+
+    @Test
+    public void testCouper(){
+        commandeCouper.execute();
+        verify(mockitoMoteur).couper();
+    }
+
+    @Test
+    public void testSelectionner(){
+        commandeSelectionner.execute();
+        verify(mockitoMoteur).selectionner(0, 0);
+    }
+
+    @Test
+    public void testSupDroite(){
+        commandeSupDroite.execute();
+        verify(mockitoMoteur).supprimerDroite();
+    }
+
+
+    @Test
+    public void testSupGauche(){
+        commandeSupGauche.execute();
+        verify(mockitoMoteur).supprimerGauche();
+    }
+
+
+    @Test
+    public void testStart(){
+        commandeStart.execute();
+        verify(mockitoEnregistreur).rec();
+    }
+
+    @Test
+    public void testStop(){
+        commandeStop.execute();
+        verify(mockitoEnregistreur).stop();
+    }
+
+    @Test
+    public void testPlay(){
+        commandePlay.execute();
+        verify(mockitoEnregistreur).play();
+    }
+
+    @Test
+    public void testUndo(){
+        commandeUndo.execute();
+        verify(mockitoManager).undo();
+    }
+
+    @Test
+    public void testRedo(){
+        commandeRedo.execute();
+        verify(mockitoManager).redo();
     }
 
 }
